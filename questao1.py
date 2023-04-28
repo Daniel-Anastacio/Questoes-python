a097e25949b706b69f05c3d909ec1497dac994e4
Daniel-Anastacio <dandangomsc18@gmail.com>
2023-04-28 19:22:41 -0300

    TUDO RESOLVIDO E DOCUMENTADO

--- questao1.py
+++ questao1.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest,re
 
 # Dado um `count` como sendo o números de donuts, retornar uma string
 # na forma "Número de donuts: <count>", caso `count` seja
@@ -7,23 +7,23 @@ def donuts(count):
   '''Usando IF para checar variável'''
   if count >=10:
     '''Maior que dez funciona melhor pois printa qualquer valor menor 10'''
-    print('many')
+    return 'Number of donuts: many'
   else:
-    print(str(count))
+    return f'Number of donuts: {count}'
     pass
-donuts(-18)
 
 
 # Outra versão
 def donutsV2(count):
+  '''USANDO WHILE COMO SE FOSSE UM IF NO FIM TUDO SE TRATA DE UMA OPERAÇÃO LÓGICA'''
   while count >= 10:
-    print('many')
+    print(f'Number of donuts: many')
     break
   while count<10:
-    print(count)
+    print('Number of donuts: ', count)
     break
     pass
-donutsV2(-18)
+
 
 
 
@@ -39,18 +39,17 @@ donutsV2(-18)
 # Caso a string `s` contenha menos que 2 caracteres,
 # retornar "" (string de cumprimento zero).
 def both_ends(s):
-  x = len(s)
-  if x > 2:
-    print(s[x-x]+s[x-x+1]+s[x-1]+s[x-2])
-  elif  x ==2:
-    print(2*(s[x-x]+s[x-x+1]))
-  elif  x <2:
-    print(4*(s[x-x]))
+  '''AQUI VAMOS SEPARAR OS CASOS POSSÍVEIS'''
+  if len(s) >= 2:
+    '''APARTIR DO TAMANHO DA STRING PODEMOS CHEGAR A SUAS SUBSTRINGS E SELECIONAR O AS DUAS INICIAIS E AS DUAS FINAIS'''
+    return s[:2]+s[len(s)-2:]
   else :
-    print('')
+    '''SE FOR MENOR QUE DOIS VAI RETORNAR UM STRING VAZIA'''
+    return ''
     pass
 
-both_ends("abcdefg")
+
+  
 # Dado uma string `s`, retornar uma string onde
 # todas as ocorrências de seu primeiro caractere
 # seja alterado para '*', exceto o primeiro caracter. Exemplo:
@@ -60,15 +59,37 @@ both_ends("abcdefg")
 # Presuma que o tamanho da string seja 1 ou mais.
 # Dica: s.replace (strA, strB) retorna uma versão da string s
 def fix_start(s):
-    pass
-
+  '''SALVAMOS A LETRA INICIAL EM UMA VARIÁVEL LOCAL'''
+  x= s[0]
+  '''TROCAMOS TODAS AS LETRAS IGUAIS A PRIMEIRA POR ASTERISCO INCLUSIVE ESTA'''
+  s = s.replace(s[0], "*")
+  '''AQUI PERCORREMOS DE MODO INDIRETO A STRING EXCETO A PRIMEIRA LETRA'''
+  for i in range(1, len(s)):
+    '''AGORA ACESSAMOS A VARIÁVEL SALVA E CONCATENAMOS FORMANDO A PALAVRA DESEJADA'''
+    x = x+s[i]
+  return x
+  pass
 
 # Dado duas string `a` e `b`,  trocar os 2 primeiros caracteres entre as variáveis
 # e retornar uma única string separada por espaço como no exemplo:
 #
 # "pezzy", "firm" ----> "fizzy perm"
 def mix_up(a, b):
-    pass
+  '''TROCAMOS AS SUBSTRINGS USANDO REPLACE, ENTRANTO SE A STRING FOR MONOCARACTERE ISSO NÃO VAI FUNCIONAR DIREITO'''
+  a,b = a.replace(a[:2], b[:2],1), b.replace(b[:2], a[:2], 1)
+  '''SOLUCIONANDO O PROBLEMA RELATADO'''
+  if len(a)<2 or len(b)<2:
+    '''COLOCA-SE OS VALORES EM UMA LISTA'''
+    list= [a,b]
+    '''DEPOIS ORDENAMOS USANDO O COMPRIMENTO DE COMO PARÂMETROS'''
+    list = sorted(list, key=len)
+    '''AGORA É POSSÍVEL SABER QUAL DAS VARIÁVEIS É A MENOR E REATRIBUIR AOS PARÂMETROS'''
+    a = list[0]
+    b = list[1]
+    '''USANDO O REPLACE O PROCESSO É FINALIZADO'''
+    a,b = a.replace(a[0], b[:2], 1), b.replace(b[:2], a[0],1)
+  return f'{a} {b}'
+  pass
 
 
 
